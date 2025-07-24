b55ce492cc6f26db52643bf4cab1b41eb1d328d8
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2025-07-24 18:31:11 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,1 +1,26 @@
+/*
+def call(Map config = [:]) {
+    def mvnCmd = config.get('mvnCmd', 'mvn clean install')
+    def javaHome = config.get('javaHome', '')
+    
+    pipeline {
+        agent any
 
+        stages {
+            stage('Build with Maven') {
+                steps {
+                    script {
+                        if (javaHome) {
+                            env.JAVA_HOME = javaHome
+                            env.PATH = "${javaHome}/bin:${env.PATH}"
+                        }
+
+                        echo "Running Maven command: ${mvnCmd}"
+                        sh "${mvnCmd}"
+                    }
+                }
+            }
+        }
+    }
+}
+*/
